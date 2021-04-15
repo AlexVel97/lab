504282f1c38e3e57d89e0714d0b54859a07aea95
VelikanovAl <strelok024@gmail.com>
2021-04-15 10:33:55 +0300

    пятая лабораторная

---
+++ components/about.php
@@ -0,0 +1,10 @@
+<section class="about container">
+  <h2 class="section-title">Привет, я <?= $name ?></h2>
+  <img class="about-image" src="<?= $image ?>" width="147" height="147" alt="<?= $name ?>">
+  <div class="about-content">
+    <p>Этот мир прогнил</p>
+    <p>И всё что остается нам</p>
+    <p>Очистить о его огнём</p>
+    <p>Примкни к нам или умри</p>
+  </div>
+</section>
