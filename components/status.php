8dfb5faa7270a7ae317b79324cf32db465c099d6
VelikanovAl <strelok024@gmail.com>
2021-04-15 10:41:51 +0300

    шестая лабораторная 1дз

--- components/status.php
+++
@@ -1,4 +0,0 @@
-<section class="status container">
-  <h2 class="section-title">Мой статус</h2>
-  <p>#МыЧумнойДоктор</p>
-</section>
--- readme.txt
+++
@@ -1,2 +0,0 @@
-hello there, this is my first repozitory on git
-rhis one is small, but it's a good job
