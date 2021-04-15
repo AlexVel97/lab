938209674d235871aa3233d265500b8ce8698e62
VelikanovAl <strelok024@gmail.com>
2021-04-15 10:57:27 +0300

    лабораторные положены в папки

---
+++ lab5/index.php
@@ -0,0 +1,15 @@
+<?php
+$title = 'Чумной Доктор';
+$name = 'Чумной доктор';
+$image = 'img/unnamed.jpg';
+$email = 'plague_doctor@vmeste.com';
+$phone = '+8 (495) 150-26-63';
+
+require('components/header.php');
+require('components/about.php');
+require('components/status.php');
+require('components/photos.php');
+require('components/news.php');
+require('components/quotes.php');
+require('components/contacts.php');
+require('components/footer.php');
