69d1635a0eeed38134e5e34b1fdfc3c0aa7478d9
VelikanovAl <strelok024@gmail.com>
2021-06-17 12:33:23 +0300

    10 лабораторная

---
+++ lab10/source/project/components/quotes.php
@@ -0,0 +1,11 @@
+<section class="quotes container">
+  <h2 class="section-title">Любимые цитаты</h2>
+  <blockquote>
+    <p>Не Бог делает мир таким, а мы — люди!</p>
+    <cite>Роршах</cite>
+  </blockquote>
+  <blockquote>
+    <p>Дело вовсе не в деньгах, главное чтобы дошёл смысл послания — что всё сгорит.</p>
+    <cite>Джокер</cite>
+  </blockquote>
+</section>
